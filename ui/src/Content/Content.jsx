bf64ab77df9c30bc30bd141f1df6bf6cca1ebc3c
Aaron Jeskie <aaronjeskie@gmail.com>
2023-05-11 20:41:46 -0400

    chore: adding new stuff

---
+++ ui/src/Content/Content.jsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react"
+
+const Content = () => {
+    
+    return (
+        <Flex direction="column">
+            <Text>Hello thar </Text>
+            <Text>Shit</Text>
+        </Flex>
+    );
+}
+
+export default Content;
---
+++ ui/src/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Flex, Heading } from "@chakra-ui/react"
+
+const NavBar = () => {
+    return (
+        <Flex w="19em" h="100%" bg='aquamarine'>
+            <Heading marginLeft="0em">
+                Application Tracker
+            </Heading>
+        </Flex>
+    )
+}
+
+export default NavBar;
